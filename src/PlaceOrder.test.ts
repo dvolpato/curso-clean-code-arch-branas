747daca61b077b0b480211f5ea512866d5261fee
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-08-12 08:49:25 -0300

    Support to place an order

---
+++ src/PlaceOrder.test.ts
@@ -0,0 +1,19 @@
+import Coupon from "./Coupon";
+import Order from "./Order";
+import PlaceOrder from "./PlaceOrder";
+
+test("Should place an order", function () {
+  // dto - data transfer object
+  const input = {
+    cpf: "754.604.580-05",
+    items: [
+      {description: "Guitarra", price: 1000, quantity: 2},
+      {description: "Amplificador", price: 5000, quantity: 1},
+      {description: "Cabo", price: 30, quantity: 3},
+    ],
+    coupon: "VALE20"
+  };
+  const placeOrder = new PlaceOrder();
+  const output = placeOrder.execute(input);
+  expect(output.total).toBe(5672);
+});
