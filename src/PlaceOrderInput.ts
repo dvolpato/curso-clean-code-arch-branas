150d1532060f43b6245e58b652b0562bec3d970d
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-08-13 07:39:48 -0300

    Add name parameters on PlaceOrderInput

--- src/PlaceOrderInput.ts
+++ src/PlaceOrderInput.ts
@@ -2,8 +2,8 @@ export default class PlaceOrderInput {
   cpf: string;
   items: any;
   coupon: string;
-  
-  constructor(cpf: string, items: any, coupon: string) {
+
+  constructor({ cpf, items, coupon }: { cpf: string, items: any, coupon: string }) {
     this.cpf = cpf;
     this.items = items;
     this.coupon = coupon;
