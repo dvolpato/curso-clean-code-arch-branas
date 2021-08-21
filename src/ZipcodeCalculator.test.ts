14662e742f277829f12e1bc3ceb49095e2a91560
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-08-21 09:18:53 -0300

    Add zip code calculator

---
+++ src/ZipcodeCalculator.test.ts
@@ -0,0 +1,7 @@
+import ZipcodeCalculatorAPIMemory from "./ZipcodeCalculatorAPIMemory";
+
+test("Should calculate the distance between two zipcodes", () => {
+  const zipcodeCalculator = new ZipcodeCalculatorAPIMemory();
+  const distance = zipcodeCalculator.calculate("11.111-111", "99.999-999");
+  expect(distance).toBe(1000);
+});
