39cd693ae5455ae6f433c065a78b040fb42bb838
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-08-24 07:53:50 -0300

    Implement Singleton on PgPromiseDatabase

--- test/integration/PgPromiseDatabase.test.ts
+++ test/integration/PgPromiseDatabase.test.ts
@@ -2,13 +2,13 @@ import PgPromiseDatabase from "../../src/infra/database/PgPromiseDatabase";
 
 describe.skip("PgPromiseDatabase", () => {
   test("should connect to the database and list the items", async () => {
-    const pgPromiseDatabase = new PgPromiseDatabase();
+    const pgPromiseDatabase = PgPromiseDatabase.getInstance();
     const items = await pgPromiseDatabase.many("select * from ccca.item", []);
     expect(items).toHaveLength(3);
   });
 
   test("should connect to the database and list one item", async () => {
-    const pgPromiseDatabase = new PgPromiseDatabase();
+    const pgPromiseDatabase = PgPromiseDatabase.getInstance();
     const item = await pgPromiseDatabase.many("select * from ccca.item where id = $1", [1]);
     expect(item.description).toBe("Guitarra");
   });
