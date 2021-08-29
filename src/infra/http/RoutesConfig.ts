56e8370d32d6f3174f987dfa5046cdf2a917bb6d
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-08-29 16:27:24 -0300

    Dependency inversion on RoutesConfig

--- src/infra/http/RoutesConfig.ts
+++ src/infra/http/RoutesConfig.ts
@@ -1,18 +1,20 @@
 import GetOrder from "../../application/GetOrder";
+import RepositoryFactory from "../../domain/factory/RepositoryFactory";
 import DatabaseRepositoryFactory from "../factory/DatabaseRepositoryFactory";
 import Http from "./Http";
 
 export default class RoutesConfig {
   http: Http;
+  repositoryFactory: RepositoryFactory;
 
-  constructor(http: Http) {
+  constructor(http: Http, repositoryFactory: RepositoryFactory) {
     this.http = http;
+    this.repositoryFactory = repositoryFactory;
   }
 
-  build () {
+  build() {
     this.http.on("get", "/orders/${code}", async (params: any, body: any) => {
-      console.log('RoutestConfig -- get -- orders/code')
-      const getOrder = new GetOrder(new DatabaseRepositoryFactory());
+      const getOrder = new GetOrder(this.repositoryFactory);
       const order = await getOrder.execute(params.code);
       return order;
     });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import DatabaseRepositoryFactory from "./infra/factory/DatabaseRepositoryFactory";
 import ExpressHttp from "./infra/http/ExpressHttp";
 import HapiHttp from "./infra/http/HapiHttp";
 import RoutesConfig from "./infra/http/RoutesConfig";
 
-//const http = new ExpressHttp();
-const http = new HapiHttp();
-const routesConfig = new RoutesConfig(http);
+const http = new ExpressHttp();
+// const http = new HapiHttp();
+const repositoryFactory = new DatabaseRepositoryFactory();
+const routesConfig = new RoutesConfig(http, repositoryFactory);
 routesConfig.build();
 http.listen(3000);
