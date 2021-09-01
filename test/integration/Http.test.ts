1829227564f7071b9042e01b175c99b1f5252ac4
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-09-01 13:22:34 -0300

    Implement API /orders - CLass #7

--- test/integration/Http.test.ts
+++ test/integration/Http.test.ts
@@ -33,3 +33,24 @@ test("Should invoke API /orders/${code}", async function () {
   expect(order.freight).toBe(310);
   expect(order.total).toBe(5982);
 });
+
+test("Should invoke API /orders", async function () {
+
+  const response = await axios({
+    url: "http://localhost:3000/orders",
+    method: "post",
+    data: {
+      cpf: "754.604.580-05",
+      zipcode: "11.111-11",
+      items: [
+        { id: "1", quantity: 2 },
+        { id: "2", quantity: 1 },
+        { id: "3", quantity: 3 },
+      ],
+      coupon: "VALE20"
+    }
+  });
+  const order = response.data;
+  expect(order.total).toBe(5982);
+  expect(order.freight).toBe(310);
+});
