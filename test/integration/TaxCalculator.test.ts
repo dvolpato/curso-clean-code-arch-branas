1363fca31d0acfbddcb021b068760ed463647375
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-09-03 13:22:26 -0300

    Use TaxCalculator entitity

--- test/integration/TaxCalculator.test.ts
+++ test/integration/TaxCalculator.test.ts
@@ -1,4 +1,5 @@
 import Item from "../../src/domain/entity/Item";
+import TaxTable from "../../src/domain/entity/TaxTable";
 import TaxCalculatorFactory from "../../src/domain/factory/TaxCalculatorFactory";
 
 describe("TaxCalculator - Normal months", () => {
@@ -7,13 +8,21 @@ describe("TaxCalculator - Normal months", () => {
 
   test("Should calculate the tax of an item Guitarra", function () {
     const item = new Item("1", "Guitarra", 1000, 100, 50, 30, 8);
-    const amount = taxCalculator.calculate(item, date);
+    const taxTables = [
+      new TaxTable("1", "default", 15),
+      new TaxTable("1", "november", 5),
+    ];
+    const amount = taxCalculator.calculate(item, taxTables);
     expect(amount).toBe(150);
   });
 
   test("Should calculate the tax of an item Cabo", function () {
     const item = new Item("3", "Cabo", 30, 10, 10, 10, 1);
-    const amount = taxCalculator.calculate(item, date);
+    const taxTables = [
+      new TaxTable("3", "default", 5),
+      new TaxTable("3", "november", 1),
+    ];
+    const amount = taxCalculator.calculate(item, taxTables);
     expect(amount).toBe(1.5);
   });
 });
@@ -24,13 +33,21 @@ describe("TaxCalculator - November", () => {
 
   test("Should calculate the tax of an item Guitarra", function () {
     const item = new Item("1", "Guitarra", 1000, 100, 50, 30, 8);
-    const amount = taxCalculator.calculate(item, date);
+    const taxTables = [
+      new TaxTable("1", "default", 15),
+      new TaxTable("1", "november", 5),
+    ];
+    const amount = taxCalculator.calculate(item, taxTables);
     expect(amount).toBe(50);
   });
 
   test("Should calculate the tax of an item Cabo", function () {
     const item = new Item("3", "Cabo", 30, 10, 10, 10, 1);
-    const amount = taxCalculator.calculate(item, date);
+    const taxTables = [
+      new TaxTable("3", "default", 5),
+      new TaxTable("3", "november", 1),
+    ];
+    const amount = taxCalculator.calculate(item, taxTables);
     expect(amount).toBe(0.3);
   });
 });
