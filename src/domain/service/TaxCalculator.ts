5a24abe9b6158b69a5e80cb3d0bbd044788de917
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-09-03 13:04:14 -0300

    Support tax calculator with polimorphic getTax method

--- src/domain/service/TaxCalculator.ts
+++ src/domain/service/TaxCalculator.ts
@@ -1,27 +1,13 @@
 import Item from "../entity/Item";
 
-export default class TaxCalculator {
+export default abstract class TaxCalculator {
   constructor() {
 
   }
+
   calculate(item: Item, date: Date) {
-    if (item.description === "Guitarra") {
-      if (date.getMonth() !== 10) {
-        return (item.price * 15) / 100;
-      }
-      else {
-        return (item.price * 5) / 100;
-      }
-    }
-
-    if (item.description === "Cabo") {
-      if (date.getMonth() !== 10) {
-        return (item.price * 5) / 100;
-      }
-      else {
-        return (item.price * 1) / 100;
-      }
-    }
-
+    return item.price * this.getTax(item) / 100;
   }
+
+  abstract getTax(item: Item): number;
 }
--- test/integration/TaxCalculator.test.ts
+++ test/integration/TaxCalculator.test.ts
@@ -1,19 +1,20 @@
 import Item from "../../src/domain/entity/Item";
-import TaxCalculator from "../../src/domain/service/TaxCalculator";
+import DefaultTaxCalculator from "../../src/domain/service/DefaultTaxCalculator";
+import NovemberTaxCalculator from "../../src/domain/service/NovemberTaxCalculator";
 
 describe("TaxCalculator - Normal months", () => {
   const date = new Date("2021-10-10");
 
   test("Should calculate the tax of an item Guitarra", function () {
     const item = new Item("1", "Guitarra", 1000, 100, 50, 30, 8);
-    const taxCalculator = new TaxCalculator();
+    const taxCalculator = new DefaultTaxCalculator();
     const amount = taxCalculator.calculate(item, date);
     expect(amount).toBe(150);
   });
 
   test("Should calculate the tax of an item Cabo", function () {
     const item = new Item("3", "Cabo", 30, 10, 10, 10, 1);
-    const taxCalculator = new TaxCalculator();
+    const taxCalculator = new DefaultTaxCalculator();
     const amount = taxCalculator.calculate(item, date);
     expect(amount).toBe(1.5);
   });
@@ -24,14 +25,14 @@ describe("TaxCalculator - November", () => {
 
   test("Should calculate the tax of an item Guitarra", function () {
     const item = new Item("1", "Guitarra", 1000, 100, 50, 30, 8);
-    const taxCalculator = new TaxCalculator();
+    const taxCalculator = new NovemberTaxCalculator();
     const amount = taxCalculator.calculate(item, date);
     expect(amount).toBe(50);
   });
 
   test("Should calculate the tax of an item Cabo", function () {
     const item = new Item("3", "Cabo", 30, 10, 10, 10, 1);
-    const taxCalculator = new TaxCalculator();
+    const taxCalculator = new NovemberTaxCalculator();
     const amount = taxCalculator.calculate(item, date);
     expect(amount).toBe(0.3);
   });
