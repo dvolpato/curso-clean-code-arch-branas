9a01617225d2b00851f0fb74bf37db382b37e1b5
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2021-09-08 13:32:25 -0300

    Add TaxTable on repo factory

--- src/infra/factory/MemoryRepositoryFactory.ts
+++ src/infra/factory/MemoryRepositoryFactory.ts
@@ -2,9 +2,11 @@ import RepositoryFactory from "../../domain/factory/RepositoryFactory";
 import CouponRepository from "../../domain/repository/CouponRepository";
 import ItemRepository from "../../domain/repository/ItemRepository";
 import OrderRepository from "../../domain/repository/OrderRepository";
+import TaxTableRepository from "../../domain/repository/TaxTableRepository";
 import CouponRepositoryMemory from "../repository/memory/CouponRepositoryMemory";
 import ItemRepositoryMemory from "../repository/memory/ItemRepositoryMemory";
 import OrderRepositoryMemory from "../repository/memory/OrderRepositoryMemory";
+import TaxTableRepositoryMemory from "../repository/memory/TaxTableRepositoryMemory";
 
 export default class MemoryRepositoryFactory implements RepositoryFactory {
   createItemRepository(): ItemRepository {
@@ -15,5 +17,8 @@ export default class MemoryRepositoryFactory implements RepositoryFactory {
   }
   createOrderRepository(): OrderRepository {
     return OrderRepositoryMemory.getInstance();
+  }
+  createTaxTableRepository(): TaxTableRepository {
+    return new TaxTableRepositoryMemory();
   }
 }
